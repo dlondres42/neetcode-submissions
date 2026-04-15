fff86dd77fbb7e8dea6e475ec9c9a2736755695c
David Londres <62063056+dlondres42@users.noreply.github.com>
2026-04-15 20:24:23 -0300

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        new_heights = sorted(heights)
+        result = 0
+
+        for i in range(len(heights)):
+            if heights[i] != new_heights[i]: result += 1
+
+        return result
